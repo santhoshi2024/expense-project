ff5228c17e1da43f71fc7e99b5c1854556d790d1
santhoshi <santhoshi6april@gmail.com>
2024-03-19 11:25:17 +0530

    Expence project... script..

---
+++ backend.sh
@@ -0,0 +1,17 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd expense
+cp backend.service /etc/systemd/system/backend.service
+rm -rf /app
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+cd /app
+unzip /tmp/backend.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
+dnf install mysql -y
+mysql -h 172.31.14.62 -uroot -pExpenseApp@1 < /app/schema/backend.sql
